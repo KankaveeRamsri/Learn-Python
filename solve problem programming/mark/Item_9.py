d3dce5c8f895c242a14c1df4d80f8cd069282f20
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2023-12-07 17:01:26 +0700

    a

--- solve problem programming/mark/Item_9.py
+++ solve problem programming/mark/Item_9.py
@@ -1,21 +1,21 @@
-while True :
-    number = int(input())
-    if number < 10 or number > 99 :
-        print("Error!, try it again")
-    else :
-        break 
+# while True :
+#     number = int(input())
+#     if number < 10 or number > 99 :
+#         print("Error!, try it again")
+#     else :
+#         break 
 
-print()
-mark = str(input())
+# print()
+# mark = str(input())
 
-reversed_number = str(number)[::-1]
+# reversed_number = str(number)[::-1]
 
-if mark == '+' :
-    answer = number + int(reversed_number)
-    print("{} {} {} = {}".format(number,mark,reversed_number,answer))
+# if mark == '+' :
+#     answer = number + int(reversed_number)
+#     print("{} {} {} = {}".format(number,mark,reversed_number,answer))
 
-elif mark == '*' :
-    answer = number * int(reversed_number)
-    print("{} {} {} = {}".format(number,mark,reversed_number,answer))
+# elif mark == '*' :
+#     answer = number * int(reversed_number)
+#     print("{} {} {} = {}".format(number,mark,reversed_number,answer))
 
 
