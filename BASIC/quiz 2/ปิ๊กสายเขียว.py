42d3906d6a6c18244fd7d7377447745f5ddbcbfc
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2023-12-14 14:04:54 +0700

    solve

--- BASIC/quiz 2/ปิ๊กสายเขียว.py
+++ BASIC/quiz 2/ปิ๊กสายเขียว.py
@@ -1,7 +1,7 @@
 def check_green(RGB) :
     if RGB[0] < 0 or RGB[1] < 0 or RGB[2] < 0 or RGB[0] > 255 or RGB[1] > 255 or RGB[2] > 255 :
         return 'Value Out of Range'
-    elif RGB[1] - ((RGB[0] + RGB[2])/2) >= 30 :
+    elif RGB[1] - ((RGB[0] + RGB[2])/2) > 30 :
         return 'Green'
     else :
         return 'Not Green'
